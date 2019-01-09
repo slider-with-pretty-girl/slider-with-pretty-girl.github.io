15928325bdea9bd9eabace89bd81f82adc0549e5
artyom12211 <43071245+artyom12211@users.noreply.github.com>
2019-01-09 12:57:20 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	if ($(window).width() <=935 ) {
+		$('.carousel-indicators').replaceWith('<ol class="carousel-indicators"> <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li><li data-target="#carousel-example-generic" data-slide-to="1"></li><li data-target="#carousel-example-generic" data-slide-to="2"></li><li data-target="#carousel-example-generic" data-slide-to="3"></li></ol>');
+	};
+
+});
+
